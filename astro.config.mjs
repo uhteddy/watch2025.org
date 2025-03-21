601f93e2772704e2ce2803ab260e35a298e2dbcb
Teddy <httpdevz@gmail.com>
2025-03-21 18:46:10 -0400

    move mountains

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from "@astrojs/tailwind";
 import cloudflare from '@astrojs/cloudflare';
+
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server', // Add this line to enable server-side rendering
+  // Add this line to enable server-side rendering
+  output: 'server',
   adapter: cloudflare(),
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  integrations: [tailwind()],
 });
